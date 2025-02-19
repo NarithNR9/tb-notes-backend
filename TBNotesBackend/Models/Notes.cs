aab98b02d9944d7342aa1a052a145a7878df64fc
NarithNR9 <narith.neymar.jr@gmail.com>
2025-02-19 22:17:07 +0700

    [add] note model

---
+++ TBNotesBackend/Models/Notes.cs
@@ -0,0 +1,13 @@
+﻿namespace TBNotesBackend.Models;
+public class Note
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public string? Content { get; set; }
+
+    public DateTime created_at { get; set; }
+
+    public DateTime? updated_at { get; set; }
+}
