5f73d7a38ffd3864830775cb3d6c539e7be48dc4
NarithNR9 <narith.neymar.jr@gmail.com>
2025-02-19 19:37:16 +0700

    [add] connection string from env

--- TBNotesBackend/Program.cs
+++ TBNotesBackend/Program.cs
@@ -1,3 +1,5 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Load .env file
+Env.Load();
+
+// Configure connection string from environment variable
+builder.Configuration["ConnectionStrings:DefaultConnection"] = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
 var app = builder.Build();
 
